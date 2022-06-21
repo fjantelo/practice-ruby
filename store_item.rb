efc201fdc8fafcc9239a51dee1c40da4569e0bbe
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-06-21 15:26:23 -0400

    call items

--- store_item.rb
+++ store_item.rb
@@ -5,3 +5,7 @@ item3 = { "name" => "cup", "color" => "white", "price" => 5 }
 item1 = { name: "paint", color: "green", price: 10 }
 item2 = { name: "toy car", color: "red", price: 3 }
 item3 = { name: "cup", color: "white", price: 5 }
+
+puts "The #{item1[color:]} #{item1[name:]} costs $#{item1[price:]}"
+puts "The #{item2[color:]} #{item2[name:]} costs $#{item2[price:]}"
+puts "The #{item3[color:]} #{item3[name:]} costs $#{item3[price:]}"
