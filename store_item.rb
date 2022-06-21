6c8acef22084a508210b8e7a35afe3c26fb68f21
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-06-21 15:00:49 -0400

    create store items

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+item1 = { "name" => "paint", "color" => "green", "price" => 10 }
+item2 = { "name" => "toy car", "color" => "red", "price" => 3 }
+item3 = { "name" => "cup", "color" => "white", "price" => 5 }
+
+item1 = { name: "paint", color: "green", price: 10 }
+item2 = { name: "toy car", color: "red", price: 3 }
+item3 = { name: "cup", color: "white", price: 5 }
